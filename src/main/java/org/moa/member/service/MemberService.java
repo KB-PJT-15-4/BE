3691c5085bf13883d466a873fd8881fbc24fb5da
Todom2 <schoolpang@naver.com>
2025-07-24 17:51:59 +0900

    Feat : 임시 구성(여행에 있는 사용자 조회)

--- src/main/java/org/moa/member/service/MemberService.java
+++ src/main/java/org/moa/member/service/MemberService.java
@@ -3,6 +3,9 @@ package org.moa.member.service;
 import org.moa.member.dto.join.MemberJoinRequestDto;
 import org.moa.member.dto.verify.MemberVerifyRequestDto;
 import org.moa.member.entity.Member;
+import org.moa.trip.dto.trip.TripMemberResponseDto;
+
+import java.util.List;
 
 public interface MemberService {
 	// boolean checkDuplicate(String email);
@@ -16,4 +19,6 @@ public interface MemberService {
 	boolean verifyJoin(MemberVerifyRequestDto dto);
 
 	Long searchUserIdByEmail(String email);
+
+	List<TripMemberResponseDto> getTripMembers(Long tripId);
 }
