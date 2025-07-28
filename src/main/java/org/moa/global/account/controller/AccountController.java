81aee7c345c2fa325667ab97c77ec2422c9fc89d
hojinLeo <201902936@hufs.ac.kr>
2025-07-28 21:26:49 +0900

    feat: 결제 기능 추가

---
+++ src/main/java/org/moa/global/account/controller/AccountController.java
@@ -0,0 +1,48 @@
+package org.moa.global.account.controller;
+
+import java.math.BigDecimal;
+
+import org.moa.global.account.dto.payment.PaymentRequestDto;
+import org.moa.global.account.dto.payment.PaymentResponseDto;
+import org.moa.global.account.service.AccountService;
+import org.moa.global.response.ApiResponse;
+import org.moa.global.security.domain.CustomUser;
+import org.moa.global.type.StatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/member/account")
+@RequiredArgsConstructor
+public class AccountController {
+
+	private final AccountService accountService;
+
+	@PostMapping("/pay")
+	public ResponseEntity<ApiResponse<PaymentResponseDto>> getAccounts(
+		@AuthenticationPrincipal CustomUser customUser,
+		@RequestBody PaymentRequestDto paymentRequestDto
+	) {
+		Long memberId = customUser.getMember().getMemberId();
+		BigDecimal amount =  paymentRequestDto.getPrice();
+		String paymentName = paymentRequestDto.getPaymentName();
+
+		log.info("AccountController ==== memberId = {}", memberId);
+		log.info("AccountController ==== amount = {}", amount);
+		log.info("AccountController ==== paymentName = {}", paymentName);
+
+		return ResponseEntity
+			.status(StatusCode.OK.getStatus())
+			.body(ApiResponse.of(accountService.makePayment(memberId, amount, paymentName)));
+	}
+}
