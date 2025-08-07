5bfeb22178625380edd537bd31a402147adfd3d6
Todom2 <schoolpang@naver.com>
2025-08-07 15:04:21 +0900

    Fix : 여행에 사용자 초대 여러명 가능하도록 수정

--- src/main/java/org/moa/trip/dto/trip/AddMemberRequestDto.java
+++ src/main/java/org/moa/trip/dto/trip/AddMemberRequestDto.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddMemberRequestDto {
     private Long tripId;
-    private Long memberId;
+    private List<Long> memberIds;
 }
