14c1aec1388be4f764a3233c16425114d1267865
Mijeong Kim <mijeong813@gmail.com>
2025-08-09 17:32:58 +0900

    refactor: Dto 보완

--- src/main/java/org/moa/trip/dto/record/TripRecordUpdateRequestDto.java
+++ src/main/java/org/moa/trip/dto/record/TripRecordUpdateRequestDto.java
@@ -1,6 +1,7 @@
 package org.moa.trip.dto.record;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
@@ -10,6 +11,7 @@ import java.util.List;
 public class TripRecordUpdateRequestDto {
     private String title;
     private String content;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate recordDate;
 
     // 클라이언트가 유지하기를 원하는 기존 이미지의 파일 이름 목록
